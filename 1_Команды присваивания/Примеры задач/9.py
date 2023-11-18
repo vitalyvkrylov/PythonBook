02610297502665a32dce4a4d3bb9ec27bc7b7cf3
vitalyvkrylov <40734709+vitalyvkrylov@users.noreply.github.com>
2023-11-18 23:40:56 +0300

    Update 9.py

--- 1_Команды присваивания/Примеры задач/9.py
+++ 1_Команды присваивания/Примеры задач/9.py
@@ -1,2 +1,2 @@
 weight_kg = 3000 * 0.45  # присвоение переменной weight_kg значения 3000 * 0.45
-print('weight_kg -> ', weight_kg)  # вывод результа (значения weight_kg)
+print('weight_kg -> ', weight_kg)  # вывод результата (значения weight_kg)
